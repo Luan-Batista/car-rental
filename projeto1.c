dae83302c5c52ad706723e4017e9571348256d61
Luan Batista <luanbatist028@gmail.com>
2024-10-19 19:41:49 -0300

    ..

--- projeto1.c
+++ projeto1.c
@@ -3,20 +3,31 @@
 
 int readFile(FILE *fp){
     fp = fopen("veiculos.dat","rb");
+
+    int qtdRecords;
+
     if(fp == NULL){
         perror("Erro ao abrir o arquivo veiculos.dat");
     }
 
     size_t recordSize = sizeof(VEHICLE);
 
+    printf("quantos registros deseja ler?\n");
+    scanf("%d", &qtdRecords);
+
     VEHICLE *vehicle;
-    vehicle = (VEHICLE*)malloc((10*sizeof(VEHICLE)));
+    vehicle = (VEHICLE*)malloc((qtdRecords)*sizeof(VEHICLE));
     if (vehicle == NULL) {
         perror("Erro ao alocar memória");
         fclose(fp);
         return -1; // Retorna erro
     }
-    size_t recordsRead = fread(&vehicle, recordSize,10, fp);
+    size_t recordsRead;
+    for(int i=0;i<qtdRecords;i++)
+    {
+       recordsRead = fread(&vehicle[i], recordSize,qtdRecords, fp);
+    }
+
     if(recordsRead < 1){
         printf("Erro ao ler o registro");
         free(vehicle);
